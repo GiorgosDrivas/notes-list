3cacff33bf09227ac3df90de6f088adbbd6b4737
George Drivas <78447254+GiorgosDrivas@users.noreply.github.com>
2023-12-05 16:22:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,48 @@ newNoteBtn.addEventListener("click", function(){
     if(noteText.value === ''){
         alert("You must fill the box");
     } else {
+
+        let col = document.createElement("div");
+        col.classList.add("col-lg-3");
+        wrap.appendChild(col);
+
+        let noteWrap = document.createElement("div");
+        noteWrap.classList.add("single-note-wrap");
+        col.appendChild(noteWrap);
+
         let li = document.createElement("li");
         li.innerHTML = noteText.value;
-        wrap.appendChild(li);
-        let span = document.createElement("span");
-        span.classList.add("delete");
-        li.appendChild(span);
+        noteWrap.appendChild(li);
 
-        li.addEventListener("click", function(){
+        const actionsWrap = document.createElement("div");
+        actionsWrap.classList.add("actions-wrap");
+        noteWrap.appendChild(actionsWrap);
+
+        let checkSpan = document.createElement("span");
+        checkSpan.innerHTML = 'Check';
+        checkSpan.classList.add("check");
+        actionsWrap.appendChild(checkSpan);
+
+        let deleteSpan = document.createElement("span");
+        deleteSpan.innerHTML = 'Delete';
+        deleteSpan.classList.add("delete");
+        actionsWrap.appendChild(deleteSpan);
+
+        let copySpan = document.createElement("span");
+        copySpan.innerHTML = 'Copy to clipboard';
+        copySpan.classList.add("copy");
+        noteWrap.appendChild(copySpan);
+
+        checkSpan.addEventListener("click", function(){
             li.classList.toggle("checked");
         });
 
-        span.addEventListener("click", function(e){
-            e.originalTarget.parentElement.remove();
+        copySpan.addEventListener("click", function(){
+            navigator.clipboard.writeText(li.innerHTML);
+        });
+
+        deleteSpan.addEventListener("click", function(e){
+            e.originalTarget.parentElement.parentElement.parentElement.remove();
         });
     }
     noteText.value = '';
